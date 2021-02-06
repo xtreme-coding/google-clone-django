34fa6384dadbbef55d03bb8c7e17c3d7be931d37
Soubhagya <soubhagyaranjanghs@gmail.com>
2021-02-06 22:49:18 +0530

    added search view

--- search/views.py
+++ search/views.py
@@ -2,3 +2,8 @@ from django.shortcuts import render
 
 def index(request):
 	return render(request, 'index.html')
+
+def search(request):
+	if request.method == 'POST':
+		search = request.POST['search']
+	return render(request, 'search.html')
